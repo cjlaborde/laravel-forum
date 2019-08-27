dadd8f423cd0a30304ba7369f1668ae431427fcc
cjlaborde <cjlaborde@gmail.com>
2019-08-27 15:29:30 +0000

    Apply fixes from StyleCI

--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -1,25 +1,24 @@
 <?php
 
-
 namespace App;
-
 
 trait RecordsActivity
 {
-
-    # Trait will listen for when Model is created and Generate new activity
+    // Trait will listen for when Model is created and Generate new activity
     protected static function bootRecordsActivity()
     {
-        if (auth()->guest()) return;
+        if (auth()->guest()) {
+            return;
+        }
 
         foreach (static::getActivitiesToRecord() as $event) {
-            # when new reccord created in database
+            // when new reccord created in database
             static::$event(function ($model) use ($event) {
                 $model->recordActivity($event);
             });
         }
 
-        # listen when reply been deleted
+        // listen when reply been deleted
         static::deleting(function ($model) {
             $model->activity()->delete();
         });
@@ -44,12 +43,12 @@ trait RecordsActivity
 
     protected function recordActivity($event)
     {
-        # timestamp will always default to now.
+        // timestamp will always default to now.
         $this->activity()->create([
             'user_id' => auth()->id(),
             'type' => $this->getActivityType($event)
         ]);
-        # replaced by activity method below.
+        // replaced by activity method below.
 //        Activity::create([
 //            'user_id' => auth()->id(),
 //            'type' => $this->getActivityType($event),
