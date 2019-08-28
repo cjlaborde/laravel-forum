4e888c6b4cf0f04bd8aecee34ec8c56ace077ca6
cjlaborde <cjlaborde@gmail.com>
2019-08-28 23:29:17 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Api/UserAvatarController.php
+++ app/Http/Controllers/Api/UserAvatarController.php
@@ -1,9 +1,12 @@
 <?php
+
 namespace App\Http\Controllers\Api;
+
 use App\Http\Controllers\Controller;
+
 class UserAvatarController extends Controller
 {
-    # store new user avatar
+    // store new user avatar
     public function store()
     {
         request()->validate([
@@ -12,6 +15,7 @@ class UserAvatarController extends Controller
         auth()->user()->update([
             'avatar_path' => request()->file('avatar')->store('avatars', 'avatar')
         ]);
+
         return response([], 204);
     }
 }
