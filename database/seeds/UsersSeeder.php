36aac72c8f49c0d06f33cc590fcd83e5cd7547b2
cjlaborde <cjlaborde@gmail.com>
2020-07-18 02:32:31 +0000

    Apply fixes from StyleCI

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -1,24 +1,25 @@
 <?php
+
  use App\User;
-use Illuminate\Database\Seeder;
+ use Illuminate\Database\Seeder;
 
-class UsersSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        User::truncate();
+ class UsersSeeder extends Seeder
+ {
+     /**
+      * Run the database seeds.
+      *
+      * @return void
+      */
+     public function run()
+     {
+         User::truncate();
 
-        factory(User::class)
+         factory(User::class)
             ->create([
                 'name' => 'John',
                 'username' => 'John',
                 'email' => 'john@gmail.com',
                 'password' => bcrypt('john@gmail.com')
             ]);
-    }
-}
+     }
+ }
